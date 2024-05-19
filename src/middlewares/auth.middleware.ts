892ddda848e29a8936254c7d3531884f1c7d9e60
Saad Ali Malik <saad.malik6366@gmail.com>
2024-05-19 15:59:20 +0500

    chore: Add authentication middleware for user routes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,1 +1,16 @@
+import { Request, Response, NextFunction } from 'express-serve-static-core';
+import jwt from 'jsonwebtoken';
 
+export const authenticateUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { access_token } = req.body;
+
+  const tokenVerified = jwt.verify(
+    access_token,
+    process.env.JWT_SECRET as string
+  );
+  console.log('Token is:', tokenVerified);
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import { Router } from 'express-serve-static-core';
 import authRouter from './auth';
 import usersRouter from './users';
+import { authenticateUser } from '../middlewares/auth.middleware';
 
 const router: Router = express.Router();
 
-router.use('/api/users', usersRouter);
+router.use('/api/users', authenticateUser, usersRouter);
 router.use('/api/auth', authRouter);
 
 export default router;
